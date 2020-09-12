77c03c1e35d5bdf4c2c140a9ed1f982fd3adfe6c
Ahsan-codelife <65127583+Ahsan-codelife@users.noreply.github.com>
2020-09-12 17:18:03 +0600

    done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDrNNmy32FqiDtZWEYJCvBdwwV4xl6w7m0",
+    authDomain: "burj-al-arab-5a030.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-5a030.firebaseio.com",
+    projectId: "burj-al-arab-5a030",
+    storageBucket: "burj-al-arab-5a030.appspot.com",
+    messagingSenderId: "529077461567",
+    appId: "1:529077461567:web:7d4ece8f5e4fe7f62bce8a"
+  };
+
+  export default firebaseConfig;
